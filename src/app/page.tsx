979daad55a0b131804bd7235d76bebe7ebf2d561
kimgunwoo <kgw8756@naver.com>
2024-11-20 00:51:29 +0900

    오류수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import styles from "../../styles/MainPage.module.css";
+import styles from "../../styles/mainpage/MainPage.module.css";
 
 export default function Home() {
   const sliderRef = useRef<HTMLDivElement>(null);
